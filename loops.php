f38c927c7e3fc191e8f2206071150cf5617fef7c
Gregory Eales <gregoeales@aol.com>
2021-01-03 13:50:09 -0800

    Create loops.php

---
+++ loops.php
@@ -0,0 +1,29 @@
+<html>
+
+<head>
+	
+<meta charset="utf-8" />
+<title> Loops!</title>
+
+</head>
+
+
+<body>
+	
+	<?php
+
+
+	$nums = array(10, 55, 22, 1002, 34);
+
+	for($i=0; $i<5; $i++)
+	{
+		echo $nums[$i];
+		echo "<br>";
+	}
+
+	?>
+
+</body>
+
+
+</html>
