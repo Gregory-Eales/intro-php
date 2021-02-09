a64c89f6f72e0412325df0a9ca53d0488ea7d57b
Gregory H Eales <gregoeales@aol.com>
2021-02-09 12:19:21 -0800

    Create variables.php

---
+++ basic/variables.php
@@ -0,0 +1,30 @@
+<html>
+
+<head>
+	
+</head>
+
+
+<body>
+
+	<?php
+
+		echo("<h1> Variables </h1>");
+		echo("<hr>");
+
+		$name = "Gregory Eales";
+		$age = 22;
+		$height = "6'2";
+		$weight = "200";
+
+		echo(" My name is $name <br>");
+		echo(" My age is $age <br>");
+		echo(" My height is $height feet<br>");
+		echo(" My weight is $weight pounds<br>");
+
+	?>
+	
+
+</body>
+
+</html>
