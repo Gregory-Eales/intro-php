9d0715710d8d177f191e9595af0ad1c1cdf5bffe
Gregory H Eales <gregoeales@aol.com>
2021-02-09 11:41:26 -0800

    refactor

--- basic/hello.php
+++ basic/hello.php
@@ -1,30 +1,21 @@
 <html>
 
+
 <head>
-	<meta charset="utf-8" />
 	<title> Hello World! </title>
+
 </head>
 
+
 <body>
+	
 	<?php
-
-	// this is a comment
-	// comments can only be used inside the php tags
-
-	$hello = "Hello, World!";
-	echo "$hello <br>";
-	print "$hello <br>"
-
-	/* 
-
-	this is a
-	muli line
-	php comment 
-
-	*/
-
-
-
+		echo "Hello, World!";
+		echo 2**3;
 	?>
+
+
+
 </body>
-<html>
+
+</html>
