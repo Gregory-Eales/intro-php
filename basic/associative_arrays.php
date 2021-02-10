902c679905dd3f70a849bfdca01cb6eea315817c
Gregory H Eales <gregoeales@aol.com>
2021-02-09 17:00:57 -0800

    Create associative_arrays.php

---
+++ basic/associative_arrays.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Associative Arrays</title>
+</head>
+<body>
+
+	<h1> Associative Arrays </h1>
+	<hr>
+
+	<?php
+	$grades = array(
+		"Jim"=>"B",
+		"Bob"=>"A"
+	);
+
+	echo $grades["Jim"];
+	echo "<br>";
+	echo $grades["Bob"];
+	?>
+
+</body>
+</html>
