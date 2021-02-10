dbb46293ad56088b4a4e8b8efb73aae1843c9de8
Gregory H Eales <gregoeales@aol.com>
2021-02-09 16:46:48 -0800

    Update checkbox.php

--- basic/checkbox.php
+++ basic/checkbox.php
@@ -1,9 +1,22 @@
+<html>
+
+
+<h1> Check Box</h1>
+<hr>
+
+<form action="checkbox.php", method="post">
+	Apples: <input type="checkbox" name="fruits[]" value="apples">
+	<br>
+	Oranges: <input type="checkbox" name="fruits[]" value="oranges">
+	<br>
+	<input type="submit">
+</form>
+
 <?php
 
-
-
-
-
-
-
+	print_r($_POST["fruits"]);
+	echo "<br>";
+	echo $_POST["fruits"][0];
 ?>
+
+</html>
