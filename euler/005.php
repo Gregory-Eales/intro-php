d0f48c5fb412fa4759598ff0c5a9095a1761194e
Gregory H Eales <gregoeales@aol.com>
2021-02-11 00:25:13 -0800

    Create 005.php

---
+++ euler/005.php
@@ -0,0 +1,25 @@
+<hr>
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+<br>
+<br>
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+<hr>
+
+<?php 
+	
+	function get_primes()
+	{
+
+	}
+
+	function get_min_multiples()
+	{
+
+	}
+
+	function get_all_divisible()
+	{
+		
+	}
+
+?>
